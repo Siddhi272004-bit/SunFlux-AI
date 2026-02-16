f8e008226a7ebde096d40cbda675e1711fada77c
SIDDHI <s51741248@gmail.com>
2026-02-17 02:22:34 +0530

    Implement load_classifier for TinyVGG model
    Add load_classifier function to initialize TinyVGG model with weights.

--- backend/app/models/load_model.py
+++ backend/app/models/load_model.py
@@ -1,1 +1,10 @@
+import torch
+from .classifier import TinyVGG
 
+def load_classifier(weights_path:str,device:str):
+  model=TinyVGG(),
+  model.load_state_dict(torch.load(weights_path,map_location=device)),
+  model.to(device), #device agnostic
+  model.eval()
+  return model #now the backend will load the model once at startup
+  
